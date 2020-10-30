aef7c17f857274ab090d1c4cc85d57fc19bcf371
ts44gj <ts44gj2020@gmail.com>
2020-10-31 02:06:45 +0900

    s

--- beginner2.js
+++ beginner2.js
@@ -1,2 +1,16 @@
+var table = document.getElementById('aaaaa');{
+    document.write("<table>");
 
-alert(document.getElementById("test"));
+ for(let i = 1 ; i < 10 ; i++) {
+    document.write("<tr>");
+
+    for(let j = 1 ; j < 10 ; j++) {
+      document.write("<td>",  i*j  , "</td>");
+    }
+
+    document.write("</tr>");
+
+ }
+
+ document.write("</table>");
+}
